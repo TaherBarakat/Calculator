77cffb48416aed5bb33e14e0b92675f9ce67a454
Taher Barakat <taher.barakat12223@gmail.com>
2022-09-25 21:19:04 +0300

    Create a primal user interface.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a, b) {
    return a + b;
 }
 function subtract(a, b) {
-   return a * b;
+   return a - b;
 }
 function multiply(a, b) {
    return a * b;
@@ -10,3 +10,20 @@ function multiply(a, b) {
 function divide(a, b) {
    return a / b;
 }
+function operator(operation, a, b) {
+   return operation(a, b);
+}
+let btn = document.querySelectorAll(".btn");
+let currentValue = document.querySelector("#current-value");
+let userInput = document.querySelector("#user-input");
+let input = "";
+let operationKey = document.querySelectorAll(".operation");
+
+btn.forEach((element) => {
+   element.addEventListener("click", () => {
+      input += element.innerText;
+      userInput.innerText = input;
+      console.log(input);
+   });
+});
+operationKey.forEach;
