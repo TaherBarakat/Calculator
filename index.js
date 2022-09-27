a3db9b149b9155bb2a475a37a841246ca64c528b
Taher Barakat <taher.barakat12223@gmail.com>
2022-09-27 05:35:03 +0300

    handle operator key double click,handle the divide operation undefined value returned and the input overflow.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function multiply(a, b) {
    return a * b;
 }
 function divide(a, b) {
+   if (b == 0) {
+      alert("the result is undefined,your calculator will be restarted!");
+      clear();
+      return "";
+   }
+
    return a / b;
 }
 function operator(operation, a, b) {
@@ -22,31 +28,37 @@ let userInput = document.querySelector("#user-input");
 let currentResult = "";
 let input = "";
 let operation;
+let operationErrorHandler;
 
 keyValue.forEach((element) => {
    element.addEventListener("click", () => {
+      handleOverFlow();
       input += element.innerText;
       userInput.innerText = input;
+      operationErrorHandler = 1;
    });
 });
 
 function choseOperation() {
-   console.log(input, " **", currentResult);
-   currentResult =
-      currentResult === ""
-         ? input
-         : operator(operation, Number(currentResult), Number(input));
+   if (operationErrorHandler == 1) {
+      console.log(input, " **", currentResult);
+      currentResult =
+         currentResult === ""
+            ? input
+            : operator(operation, Number(currentResult), Number(input));
 
-   if (this.innerText == "/") operation = divide;
-   else if (this.innerText == "x") operation = multiply;
-   else if (this.innerText == "+") operation = add;
-   else if (this.innerText == "–") operation = subtract;
+      if (this.innerText == "/") operation = divide;
+      else if (this.innerText == "x") operation = multiply;
+      else if (this.innerText == "+") operation = add;
+      else if (this.innerText == "–") operation = subtract;
 
-   lastValue.innerText = currentResult + this.innerText;
+      lastValue.innerText = currentResult + this.innerText;
 
-   input = "";
-   userInput.innerText = "0";
-   console.log(input, " **", currentResult);
+      input = "";
+      userInput.innerText = "0";
+      console.log(input, " **", currentResult);
+      operationErrorHandler = 0;
+   }
 }
 
 operationKey.forEach((element) => {
@@ -54,14 +66,15 @@ operationKey.forEach((element) => {
 });
 
 // clear and delete functions
-
-document.querySelector("#clear").addEventListener("click", () => {
+function clear() {
    currentResult = "";
    input = "";
    operation = undefined;
    userInput.innerText = "0";
    lastValue.innerText = "";
-});
+}
+
+document.querySelector("#clear").addEventListener("click", clear);
 
 document.querySelector("#delete").addEventListener("click", () => {
    userInput.innerText = userInput.innerText.slice(0, -1);
@@ -82,3 +95,14 @@ document.querySelector("#equal").addEventListener("click", () => {
    currentResult = "";
    console.log(input, " **", currentResult);
 });
+
+handleOverFlow();
+//handle the overflow
+function handleOverFlow() {
+   if (Number(userInput.innerText) >= 9999999999) {
+      alert(
+         "You have overbaked the value limit.your calculator will be restarted!"
+      );
+      clear();
+   }
+}
